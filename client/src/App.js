1c80cde368fa0a4c149d67e59b9f057917ef877e
Alex Kaplan <Kaplanalex04@gmail.com>
2019-12-22 11:53:36 -0800

    Custom client initialization with spotify auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <p>Let's start tagging!</p>
+      <button
+        onClick={() =>
+          window.location.replace("http://localhost:5000/auth/login")
+        }
+      >
+        Login with spotify
+      </button>
+    </div>
+  );
+};
 
 export default App;
