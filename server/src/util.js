f3e8a30b3884b25ab710988ad37b25a9d334075e
Alex Kaplan <Kaplanalex04@gmail.com>
2019-12-22 00:24:33 -0800

    Functional jwt strategy

--- server/src/util.js
+++ server/src/util.js
@@ -2,6 +2,6 @@ export const getSpotifyAuthHeader = () => {
   const spotifyCodes = `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`;
   const encoded =
     "Basic " +
-    Buffer.alloc(spotifyVales.length, spotifyCodes).toString("base64");
+    Buffer.alloc(spotifyCodes.length, spotifyCodes).toString("base64");
   return encoded;
 };
